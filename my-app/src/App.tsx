a11a4fd62df7baaf30672bb63563299d76e32291
KT1024-kei <kt0120katu@gmail.com>
2024-11-06 00:09:18 +0900

    refactor: supabaseのサンプルAPIの切り離し

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,19 @@
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_PROJECT_URL || "",
-  import.meta.env.VITE_ANON_KEY || ""
-);
+import { Country, getCountries } from "./database/countries/getCountries";
 
 function App() {
-  const [countries, setCountries] = useState<any[]>([]);
+  const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
-    getCountries();
-  }, []);
+    const fetchData = async () => {
+      const res = await getCountries();
+      if (res) {
+        setCountries(res);
+      }
+    };
 
-  const getCountries = async () => {
-    const { data: countries, error } = await supabase
-      .from("countries")
-      .select("*");
-    if (error) {
-      console.error(error);
-    } else {
-      setCountries(countries || []);
-    }
-  };
+    fetchData();
+  }, []);
 
   return (
     <ul>
---
+++ my-app/src/database/countries/getCountries.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+
+export type Country = {
+  id: number;
+  name: string;
+};
+
+export const getCountries = async (): Promise<Country[] | undefined> => {
+  const supabase = createClient(
+    import.meta.env.VITE_PROJECT_URL || "",
+    import.meta.env.VITE_ANON_KEY || ""
+  );
+
+  const { data: countries, error } = await supabase
+    .from("countries")
+    .select("*");
+  if (error) {
+    console.error(error);
+    return;
+  } else {
+    return countries || [];
+  }
+};
