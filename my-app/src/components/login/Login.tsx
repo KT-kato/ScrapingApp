44cd651abea7ea3c4ee3916b50dc6f1d772368d0
KT1024-kei <kt0120katu@gmail.com>
2024-12-10 23:13:47 +0900

    ログインページとサインアップページを作成

--- my-app/src/components/login/Login.tsx
+++ my-app/src/components/login/Login.tsx
@@ -1,17 +1,53 @@
-import { Button, Card, CardBody, CardTitle, Form, Input, Label } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  Form,
+  Input,
+  Label,
+} from "reactstrap";
 
 import styles from "./Login.module.scss";
 import { Page } from "../page/Page";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { login, selectSessionStatus } from "../../slices/sessionSlices";
+import { useDispatch } from "../../slices/store";
+import { useSelector } from "../../slices/store";
+import { useNavigate } from "react-router";
+import { shallowEqual } from "react-redux";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const { isRequesting, errorMessage } = useSelector(
+    selectSessionStatus,
+    shallowEqual
+  );
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {
-    console.log("email", email);
-    console.log("password", password);
+  const handleSubmit = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    dispatch(login({ email, password }));
+    setIsSubmitted(true);
   };
+
+  useEffect(() => {
+    if (!isSubmitted || isRequesting) {
+      return;
+    }
+    if (errorMessage) {
+      alert(errorMessage.message);
+      setIsSubmitted(false);
+      return;
+    }
+    navigate("/home");
+    setIsSubmitted(false);
+  }, [isSubmitted, isRequesting, errorMessage, navigate]);
+
   return (
     <Page>
       <Card className={styles.loginContainer}>
@@ -46,7 +82,9 @@ export const Login = () => {
             <div className={styles.registerContainer}>
               <span>Don't have an account?</span>
             </div>
-            <Button color="link">Register</Button>
+            <Button color="link" onClick={() => navigate("/signup")}>
+              Register
+            </Button>
           </Form>
         </CardBody>
       </Card>
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./slices/store.ts";
 import { Login } from "./components/login/Login.tsx";
 import { Home } from "./components/home/Home.tsx";
+import { Signup } from "./components/signup/Signup.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
