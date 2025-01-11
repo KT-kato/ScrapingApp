0f7b1b535af0c4292594ed1539cd4b208ba1c1e0
KT1024-kei <keisuke.kato@access-company.com>
2025-01-11 15:07:31 +0900

    imp: ブランドの一覧とブランドの型番の一覧を返す関数を実装

--- my-app/supabase/functions/ebay/getBlandList.ts
+++ my-app/supabase/functions/ebay/getBlandList.ts
@@ -1,127 +1,29 @@
-import { corsHeaders } from "../common/utils.ts";
-import axios from "npm:axios";
-import { CheerioAPI, load } from "npm:cheerio";
-import type { Element } from "npm:domhandler";
+import { corsHeaders, supabaseClient } from "../common/utils.ts";
+import { blandListType } from "./utils/types.ts";
 
-const replaceQueryParametersForEbay = (url: string) => {
-  const query = url.split("?")[1];
-  const queryParameters = new URLSearchParams(query);
-  const ebayQueryParameters = {
-    _nkw: queryParameters.get("keywords") || "", // 商品のキーワード（検索欄の内容）
-    _sacat: queryParameters.get("categoryId") || "", // 商品の型番を指定
-    LH_Complete: queryParameters.get("completed") || "", // 完了した商品かどうか 0, 1
-    LH_Sold: queryParameters.get("sold") || "", // 販売済みの商品かどうか 0, 1
-    _salic: queryParameters.get("country") || "", // 国ID （日本：104）
-    LH_PrefLoc: queryParameters.get("location") || "", // 発送元 （日本：98、米国：３）
-    LH_LocatedIn: "1",
-    _sop: "16",
-    _dmd: "2",
-    _ipg: "200",
-    _fcid: "1",
-    rt: "nc",
-  };
-  const ebayQuery = new URLSearchParams(ebayQueryParameters).toString();
-  return ebayQuery;
-};
-
-const getItemCount = ($: CheerioAPI) => {
-  const itemCount = $(".srp-main-content")
-    .find("div > div > div > div > div")
-    .find(".srp-controls__count-heading")
-    .find("span")
-    .last()
-    .text();
-  return itemCount;
-};
-
-const getItemName = ($: CheerioAPI, element: Element) =>
-  $(element).find("a").find(".s-item__title").children("span").text();
-const getItemPriceYen = ($: CheerioAPI, element: Element) =>
-  $(element)
-    .find("div")
-    .find("div")
-    .find(".s-item__price")
-    .text()
-    .replace("$", "")
-    .replace(".", "")
-    .replace(",", "");
-const getItemShippingCost = ($: CheerioAPI, element: Element) =>
-  $(element)
-    .find("div")
-    .find("div")
-    .find(".s-item__shipping")
-    .text()
-    .replace("$", "")
-    .replace(".", "")
-    .replace(",", "")
-    .replace(" shipping", "")
-    .replace("+", "")
-    .replace("Free", "");
-
-type itemProp = {
-  itemName: string;
-  itemPrice: string | number;
-  itemSippingCost: number;
-};
-
-const getItemList = async (ebayItemUrl: string) => {
-  const items: itemProp[] = [];
+export const getBlandList = async () => {
   try {
-    const response = await axios.get(ebayItemUrl);
-    const $ = load(response.data);
-    const itemCount = getItemCount($);
-    if (itemCount === "") {
-      return items;
-    }
-    const itemInfoElements = $(".s-item__info");
-    itemInfoElements.map((index, element) => {
-      if (index > Number(itemCount) + 1) return;
-      $(element).map((_, element) => {
-        const itemName = getItemName($, element);
-        const itemPriceYen = Number(
-          getItemPriceYen($, element).replace(/\D/g, "")
-        );
-        const itemShippingCost = getItemShippingCost($, element).replace(
-          /\D/g,
-          ""
-        );
-
-        if (itemName == "Shop on eBay") {
-          return items;
-        }
-
-        const itemSippingCostYen =
-          itemShippingCost === "" ? 0 : Number(itemShippingCost);
-
-        items.push({
-          itemName,
-          itemPrice:
-            itemShippingCost === ""
-              ? String(itemPriceYen)
-              : String(itemPriceYen + itemSippingCostYen),
-          itemSippingCost: itemSippingCostYen,
-        });
-      });
-    });
-    return items;
-  } catch (error) {
-    console.error("Error in getItemDetails: ", error);
-    return items;
-  }
-};
-
-// 特定のキーワードとカテゴリーのIDを元に、ebayの商品リストを取得する
-export const getBlandList = async (req: Request) => {
-  const ebayQuery = replaceQueryParametersForEbay(req.url);
-  const ebayUrl = `https://www.ebay.com/sch/i.html?${ebayQuery}`;
-  const items = await getItemList(ebayUrl);
-  return new Response(
-    JSON.stringify({
-      url: ebayUrl,
-      items,
-    }),
-    {
+    const data = await supabaseClient.from("item_bland").select("*");
+    const responseData = data.data !== null
+      ? data.data.map((item: blandListType) => {
+        return {
+          id: item.id,
+          blandName: item.bland_name,
+        };
+      })
+      : [];
+    return new Response(JSON.stringify({ blandList: responseData }), {
       headers: { "Content-Type": "application/json", ...corsHeaders },
-    }
-  );
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: error,
+      }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json", ...corsHeaders },
+      },
+    );
+  }
 };
